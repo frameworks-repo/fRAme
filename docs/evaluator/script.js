379cdbe8f799c1882dc1d476b9f28c959bbefbee
Andrea Bombarda <a.bombarda@studenti.unibg.it>
2022-04-27 11:36:28 +0200

    Update script.js

--- docs/evaluator/script.js
+++ docs/evaluator/script.js
@@ -2,15 +2,13 @@
 var options = ["Not valid","Satisfied","Improvable (low effort)","Improvable (high effort)", "Does not apply"];
 var colors = ['#FFFFFF', '#00FF00', '#FFFF5A' ,'#FFA500' ,'#808080'];
 
-var Configurability = ["Static Configuration", "Start-up Configuration", "Run-time Configuration","Run-time Self Configuration","Autonomous Configuration"];
-var Adaptability = ["No Adaptation","Recognition of the need for adaptation", "Adaptation of individual components/parameters/tasks", "Process chain adaptation / Multiple parameters adaptation", "Communicated component/parameter adaptation"];
-var Dependability = ["No dependability", "Mean failure dependability", "Fails Safe", "Failure Recovery", "Graceful Degradation", "Task dependability", "Mission dependability", "Predictive dependability"];
 var Autonomy = ["No autonomy", "Basic action", "Basic decisional autonomy", "Continuous basic decisional autonomy", "Simple autonomy without environment model", "Simple autonomy with environment model", "Task autonomy", "Constrained task autonomy", "Multiple task autonomy", "Dynamic autonomy", "Mission oriented autonomy", "Distributed autonomy"];
 var Interaction = [
     ["No interaction", "Direct control", "Direct physical interaction", "Task selection", "Traded autonomy", "Task sequence control", "Supervised autonomy", "Task sequence control", "Mission Goal setting"],
     ["No feedback", "Visual feedback", "Vision data feedback", "Haptic feedback", "Tele-presence"],
     ["No interaction", "Communication of own status", "Communication of task status", "Communication of environment information", "Team communication", "Team coordination", "Capability Communication"]
 ];
+
 
 const descriptions = new Map();
 descriptions.set("Adaptability - No Adaptation", "The system does not alter its operating behavior in response to experience gained over time."); 
@@ -54,15 +52,15 @@ function fillOptions(cell, element){
     }
 }
 // el: the element to be added
-function fillRow(el, tbody, abilityName){
+function fillRow(tbody, abilityLevel){
     let row = document.createElement('tr');
     // first row the element
     let cell_1 = document.createElement('td');
     // add the description
-    cell_1.innerHTML = "<details> <summary>"+ el + "</summary>" + descriptions.get(abilityName + " - " + el) + "</details>";
+    cell_1.innerHTML = "<details> <summary>"+ abilityLevel.levelName + "</summary>" + abilityLevel.levelDescription + "</details>";
     // options levels
     let cell_2 = document.createElement('td');
-    fillOptions(cell_2, el);
+    fillOptions(cell_2, abilityLevel.levelName);
     // text area for the scenario
     let cell_3 = document.createElement('td');
     var ta = document.createElement("TEXTAREA");
@@ -73,20 +71,23 @@ function fillRow(el, tbody, abilityName){
     row.appendChild(cell_3);
     tbody.appendChild(row);
 }
-// capability: la stringa
-// livels: l'array con tutti i livelli
-function fillTable(capability, levels) {
-    //var h2 = document.createElement ("h2");
-    //h2.innerHTML = capability;
-    //document.getElementById(capability).appendChild(h2);
-    // <h2>capability:</h2>
+
+function fillTable(abilityName, abilityDescription, hasSubAbilities, abilityLevels) {
+
     let table = document.createElement('table');
     let thead = document.createElement('thead');
     let tbody = document.createElement('tbody');
+    let tDescription = document.createElement('p');
     table.appendChild(thead);
     table.appendChild(tbody);
+
+    // Initial description for the ability
+    tDescription.innerHTML = abilityDescription;
+    document.getElementById(abilityName).appendChild(tDescription);
+
     // Adding the entire table to the body tag
-    document.getElementById(capability).appendChild(table);
+    document.getElementById(abilityName).appendChild(table);
+
     // Creating and adding data to first row of the table
     let row_1 = document.createElement('tr');
     let heading_1 = document.createElement('th');
@@ -99,15 +100,23 @@ function fillTable(capability, levels) {
     row_1.appendChild(heading_2);
     row_1.appendChild(heading_3);
     thead.appendChild(row_1);
-    // create a row for every level
-    levels.forEach(element => fillRow(element,tbody, capability));
+
+    // Create a row for every level
+    if (!hasSubAbilities) {
+        for (let i=0; i<abilityLevels.length; i++) {
+            fillRow(tbody, abilityLevels[i]);
+        }
+    } else {
+        // TODO: Add handling of abilities with subabilities
+    }
 }
 
-function fillTables(){
-    fillTable("Configurability", Configurability);
-    fillTable("Dependability", Dependability);
-    fillTable("Adaptability", Adaptability);
-    fillTable("Autonomy", Autonomy);
+async function fillTables(){
+
+    var data = await getJSONAbilities();
+    for (let i=0; i<data.length; i++) {
+        fillTable(data[i].abilityName, data[i].abilityDescription, data[i].hasSubAbilities, data[i].abilityLevels);
+    }
 }
 
 
@@ -175,4 +184,10 @@ function pdf() {
     //w.document.write(document.getElementById("canvas").)
     w.document.close()
     w.print();
+}
+
+async function getJSONAbilities() {
+    const response = await fetch("https://raw.githubusercontent.com/fmselab/ADVISOR/main/docs/evaluator/abilities.json");
+    const json = await response.json();
+    return json.data;
 }
